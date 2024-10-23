2b97ff311e984cee1d2fabb1c03849c9b82c6a06
Alex Scripcariu <heygle3@gmail.com>
2024-10-23 11:38:32 +0100

    finished starter

---
+++ projects/random-module/starter.py
@@ -0,0 +1,15 @@
+import random
+
+num = random.random()
+print(num)
+
+num = random.randint(0, 100)
+print(num)
+
+
+num = random.randrange(0, 100, 5)
+print(num)
+
+colour = random.choice(["red", "blue", "black"])
+print(colour)
+
