8f9b6f619c78764a657125093b9919463c5d930b
Alex Scripcariu <alexscripcariu41209@gmail.com>
2024-10-27 10:26:23 +0000

    added challenge_22.py

---
+++ projects/strings/challenges/challenge_22.py
@@ -0,0 +1,6 @@
+first_lower = input("Enter your name in lowercase: ").lower()
+last_lower = input("Enter your last name in lowercase: ").lower()
+full_name = (first_lower + " " + last_lower).title()
+
+
+print(full_name)
