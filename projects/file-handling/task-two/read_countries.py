d36f27483706dd3e9e25bc17159b963e4db1e5f5
Alex Scripcariu <alexscripcariu41209@gmail.com>
2025-01-08 17:54:14 +0000

    finished task three

--- projects/file-handling/task-two/read_countries.py
+++ projects/file-handling/task-two/read_countries.py
@@ -2,8 +2,8 @@ from pathlib import Path
 
 path = Path("D:\\git-projects\\Computer-science-work\\projects\\file-handling\\task-two\\read_countries.py").parent.absolute()
 
-with open(str(path) + "\\Countries.txt", "w") as file:
-  file.writelines(["Romania\n", "Belgium\n", "France\n", "Netherlands\n"])
+# with open(str(path) + "\\Countries.txt", "w") as file:
+#   file.writelines(["Romania\n", "Belgium\n", "France\n", "Netherlands\n"])
 
 
 with open(str(path) + "\\Countries.txt", "r") as file:
