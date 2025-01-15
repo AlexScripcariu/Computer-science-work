45519da35108817626ca587758d4a58a22a1ed72
AlexScripcariu <alexscripcariu41209@gmail.com>
2025-01-15 11:57:21 +0000

    Finished readlines.py

---
+++ projects/file-handling/file-handling-extension/task-one/filereadlines.py
@@ -0,0 +1,6 @@
+file_path = "C:\\Users\\21scripcariua-wes\\Computer-science-work\\projects\\file-handling\\file-handling-extension\\task-one\\Hello_World.txt"
+
+with open(file_path, "r") as file:
+    print(file.readline())
+    print(file.readline())
+    print(file.readline())
